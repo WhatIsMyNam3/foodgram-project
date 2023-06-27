ff8378e0ee2b35f26872e82361d507f0f9aa1339
Roman Leskov <somebodyoncetoldmetheworl12@gmail.com>
2023-06-27 16:17:28 +1000

    commit

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -25,8 +25,26 @@ class UserSerializer(DjoserUserSerializer):
             'username',
             'first_name',
             'last_name',
-            'is_subscribed'
+            'is_subscribed',
+            'password'
         ]
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'required': True},
+            'is_subscribed': {
+                'read_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
     def get_is_subscribed(self, obj):
         request = self.context.get('request')

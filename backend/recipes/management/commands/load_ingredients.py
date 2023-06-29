e1b3917e6e9e6b2eb70ce0dfdf6b3858f8dd6009
Roman Leskov <somebodyoncetoldmetheworl12@gmail.com>
2023-06-29 21:45:22 +1000

    flake8

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -8,10 +8,10 @@ csv_files = {
     Ingredient: '../data/ingredients.json'
 }
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        i = 0
         with open(csv_files[Ingredient], encoding='utf-8') as f:
             jsondata = json.load(f)
             if 'measurement_unit' in jsondata[0]:
@@ -19,9 +19,6 @@ class Command(BaseCommand):
                     if not Ingredient.objects.filter(
                        name=line['name'],
                        measurement_unit=line['measurement_unit']).exists():
-                        i+=1
-                        print(i)
-                        print(line)
                         Ingredient.objects.create(
                             name=line['name'],
                             measurement_unit=line['measurement_unit']

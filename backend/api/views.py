7e4706ff7a84e1dc0b6570d3ebb6c768014ef742
Roman Leskov <somebodyoncetoldmetheworl12@gmail.com>
2023-07-03 15:57:21 +1000

    pep8

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet as DjoserUserViewSet
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
@@ -145,7 +144,7 @@ class RecipeViewSet(ModelViewSet):
         detail=True,
         methods=['post', 'delete'],
         permission_classes=[IsAuthenticated, ],
-        pagination_class = None
+        pagination_class=None
     )
     def shopping_cart(self, request, pk):
         if request.method == 'POST':
